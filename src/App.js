f99da8e770217abfe0fdb3e23c044ff06ab22b9d
rishabh <rishabhsaini014@gmail.com>
2022-09-07 01:19:19 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 import InfoBox from "./InfoBox";
 import LineGraph from "./LineGraph";
 import Table from "./Table";
-import { sortData, prettyPrintStat } from "./util";
+ import { sortData, prettyPrintStat } from "./util";
 import numeral from "numeral";
 import Map from "./Map";
 import "leaflet/dist/leaflet.css";
@@ -136,3 +136,4 @@ const App = () => {
 };
 
 export default App;
+
